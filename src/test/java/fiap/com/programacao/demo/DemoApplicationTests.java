35a05fd5f94adc9e3ea93b06ca09922a1e9a4ef3
Thiago S Adriano <tadriano.net@gmail.com>
2023-08-04 19:10:01 -0300

    init

---
+++ src/test/java/fiap/com/programacao/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package fiap.com.programacao.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
